224f008d1db071ea556c653839ed0ab38f408186
nikita.aleksandrov <mathymath24@gmail.com>
2022-03-07 16:59:57 +0300

    Initial commit

---
+++ __englishdart_fss_or_kr.py
@@ -0,0 +1,18 @@
+import time
+import json
+from englishdart_fss_or_kr import *
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    a = Handler()
+    # final_data = a.Execute('MDA0MDAwPz1MT1RURSBGaW5lIENoZW1pY2Fs',
+    #                        'Financial_Information', '', '')
+    # final_data = a.Execute('samsung_electronics','','','')
+    # final_data = a.Execute('MDA0MDAwPz1MT1RURSBGaW5lIENoZW1pY2Fs', 'graph:shareholders', '', '')
+    final_data = a.Execute('MDA1OTMwPz1TQU1TVU5HIEVMRUNUUk9OSUNT=',
+                            'Financial_Information','','')
+    print(json.dumps(final_data, indent=4))
+
+    elapsed_time = time.time() - start_time
+    print('\nTask completed - Elapsed time: ' + str(round(elapsed_time, 2)) + ' seconds')
